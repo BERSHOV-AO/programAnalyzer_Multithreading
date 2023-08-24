129c4b67555260b67d37a6aade13defba8e9463f
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-08-24 15:36:31 +0300

    Initial commit, task programm analyzer

---
+++ src/main/java/Main.java
@@ -0,0 +1,82 @@
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class Main {
+
+    public static BlockingQueue<String> queue1 = new ArrayBlockingQueue<>(100);
+    public static BlockingQueue<String> queue2 = new ArrayBlockingQueue<>(100);
+    public static BlockingQueue<String> queue3 = new ArrayBlockingQueue<>(100);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread threadAdd = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                queue1.add(generateText("abc", 100_000));
+                queue2.add(generateText("abc", 100_000));
+                queue3.add(generateText("abc", 100_000));
+            }
+        });
+
+        threadAdd.start();
+        threadAdd.join();
+
+        Thread threadChar_A = new Thread(() -> {
+            System.out.println("\n\nСтрока с максимальным количеством символов - a: " + MaxCharStringQueue(queue1,
+                    'a'));
+        });
+
+        Thread threadChar_B = new Thread(() -> {
+            System.out.println("\n\nСтрока с максимальным количеством символов - b: " + MaxCharStringQueue(queue2,
+                    'b'));
+        });
+
+        Thread threadChar_C = new Thread(() -> {
+            System.out.println("\n\nСтрока с максимальным количеством символов - c: " + MaxCharStringQueue(queue3,
+                    'c'));
+        });
+
+        threadChar_A.start();
+        threadChar_B.start();
+        threadChar_C.start();
+
+        threadChar_C.join();
+        threadChar_B.join();
+        threadChar_A.join();
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+
+    public static String MaxCharStringQueue(BlockingQueue<String> queue, char targetChar) {
+
+        String maxCharString = "";
+        int maxCharCount = 0;
+
+        for (String str : queue) {
+            int charCount = countChar(str, targetChar);
+
+            if (charCount > maxCharCount) {
+                maxCharCount = charCount;
+                maxCharString = str;
+            }
+        }
+        return maxCharString;
+    }
+
+    public static int countChar(String str, char targetChar) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == targetChar) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
